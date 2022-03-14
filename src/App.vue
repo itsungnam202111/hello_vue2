f3fbd6298b18bbbedd02f5743816142fc8458100
itsungnam202111 <101399696+itsungnam202111@users.noreply.github.com>
2022-03-14 21:15:10 +0900

    4
    44

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="반갑습니다. Vue 애플리케이션 입니다."/>
-  <HelloWorld msg2="반갑습니다!!!!"/>
+  <HelloWorld msg="반갑습니다. Vue!! 애플리케이션"/>
+  <HelloWorld2 name="한사람" age="33" gender="true"/>
+  <HelloWorld2 name="두사람" age="18" gender="false"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld2 from './components/HelloWorld2.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloWorld2
   }
 }
 </script>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h3>{{ name }}({{age}}세, {{gender=='true' ? "남자" : "여자"}})</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld2",
+  props: {
+    name: String,
+    age: Number,
+    gender: Boolean
+  },
+};
+</script>
+
